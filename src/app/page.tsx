490ddba8faa15b9ffd6856c200f80b8aaacff63a
Areeba khan <arkhan.2003k@gmail.com>
2025-12-02 02:11:26 -0800

    login done

---
+++ src/app/page.tsx
@@ -0,0 +1,25 @@
+// import HeroSection from "@/components/sections/HeroSection";
+// import FAQSection from "@/components/sections/FAQSection";
+// import Features from "@/components/sections/Features";
+
+import LoginPage from "./login/page";
+
+// import Contact from "@/components/sections/Contact";
+
+// import Kind from "@/components/sections/Kind";
+// import Productive from "@/components/sections/Productive";
+// import Small from "@/components/sections/Small";
+export default function HomePage() {
+  return (
+    <main className="relative">
+      <LoginPage />
+      {/* <HeroSection />
+      <Features />
+      <Small />
+      <Productive />
+      <Kind />
+      <FAQSection />
+      <Contact /> */}
+    </main>
+  );
+}
