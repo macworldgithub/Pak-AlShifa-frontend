00b7670d033f19e79fef79f8a6a301c50c202e2a
Ayesha Fahim <fahim.ayesha000@gmail.com>
2025-12-22 04:37:07 -0800

    set layout

--- src/components/layout/ClientLayout.tsx
+++ src/components/layout/ClientLayout.tsx
@@ -1,7 +1,8 @@
 "use client";
-import { ReactNode, useState } from "react";
+import { ReactNode, useState, useEffect } from "react";
 import Sidebar from "./Sidebar";
 import Navbar from "./Navbar";
+import { usePathname } from "next/navigation";
 
 interface ClientLayoutProps {
   children: ReactNode;
@@ -9,20 +10,30 @@ interface ClientLayoutProps {
 
 export default function ClientLayout({ children }: ClientLayoutProps) {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const pathname = usePathname();
 
   const toggleSidebar = () => setIsSidebarOpen(!isSidebarOpen);
+
+  // Pages that should NOT show sidebar/navbar
+  const noLayoutPages = ["/login", "/"];
+
+  const shouldShowLayout = !noLayoutPages.includes(pathname);
+
+  if (!shouldShowLayout) {
+    return <>{children}</>;
+  }
 
   return (
     <div className="flex h-screen bg-gray-100">
       {/* Sidebar */}
       <Sidebar isOpen={isSidebarOpen} onClose={() => setIsSidebarOpen(false)} />
 
-      {/* Main Content - Shift right on desktop to account for fixed sidebar width */}
+      {/* Main Content */}
       <div className="flex-1 flex flex-col overflow-hidden md:ml-[240px]">
-        {/* Navbar with toggle */}
+        {/* Navbar */}
         <Navbar onToggleSidebar={toggleSidebar} />
 
-        {/* Page Content - Consistent padding on all sides for all pages */}
+        {/* Page Content */}
         <main className="flex-1 overflow-y-auto p-6 bg-gray-100">
           {children}
         </main>
