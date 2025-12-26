beb1bbe186d219b1d4fccba75e5377e32dd540a7
jawwadkareem <karimjawwad09@gmail.com>
2025-12-26 09:10:54 +0500

    Frontend UI updated

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 // src/config.ts
-export const BACKEND_URL = 'http://localhost:7008';
-// export const BACKEND_URL = 'https://pak-alshifa-backend-production.up.railway.app';
+// export const BACKEND_URL = 'http://localhost:7008';
+export const BACKEND_URL = 'https://pak-alshifa-backend-production.up.railway.app';
